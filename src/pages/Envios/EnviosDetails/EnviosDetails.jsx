aeb8dfa7cbce0097b2e0448b80ac76702a48bd40
Weishler Berman <52388760+ilcreatore32@users.noreply.github.com>
2021-10-08 12:58:35 -0400

    Enable modes on EnviosDetails
    Conditional rendering added to view if is accepted or not.

--- src/pages/Envios/EnviosDetails/EnviosDetails.jsx
+++ src/pages/Envios/EnviosDetails/EnviosDetails.jsx
@@ -26,6 +26,9 @@ import {
   Table,
   Collapse,
   TableHead,
+  FormGroup,
+  FormControlLabel,
+  Switch,
 } from "@mui/material";
 
 /* Material UI Icons */
@@ -39,6 +42,7 @@ function EnviosDetails() {
   const [open, setOpen] = useState(false);
   const [Offer, setOffer] = useState(false);
   const [SelectedOffer, setSelectedOffer] = useState(null);
+  const [TransportistaAccepted, setTransportistaAccepted] = useState(true);
 
   const handleOffer = () => {
     setOffer(true);
@@ -52,11 +56,26 @@ function EnviosDetails() {
     setSelectedOffer(e.target.value);
   };
 
+  const handleChange = (event) => {
+    setTransportistaAccepted(event.target.checked);
+  };
+
   return (
     <>
       <Box sx={{ display: "flex", justifyContent: "space-between" }}>
         <Box sx={{ width: "100%", padding: ".5rem" }}>
           <Paper sx={{ padding: "1rem" }} variant="outlined">
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Switch
+                    checked={TransportistaAccepted}
+                    onChange={handleChange}
+                  />
+                }
+                label={TransportistaAccepted ? "Accepted" : "Refuse"}
+              />
+            </FormGroup>
             <Box>
               <Typography align="center" variant="h4" component="h1">
                 Detalles del Envio
@@ -142,99 +161,155 @@ function EnviosDetails() {
                   </Table>
                 </TableContainer>
               </Box>
-              <Box
-                sx={{
-                  padding: "1rem",
-                }}
-              >
-                <List
-                  sx={{
-                    width: "100%",
-                    bgcolor: "background.paper",
-                  }}
-                  component={Paper}
-                  variant="outlined"
-                >
-                  <ListSubheader
+              {TransportistaAccepted ? (
+                <>
+                  <Box
                     sx={{
                       display: "flex",
-                      alignContent: "center",
+                      alignItems: "baseline",
+                      gap: 0.5,
                       justifyContent: "center",
-                      alignItems: "center",
+                      margin: "1rem 0",
                     }}
                   >
-                    Servicios de Transporte Ofertados{" "}
-                    <IconButton onClick={handleOffer}>
-                      <BookmarkAddTwoToneIcon color="secondary" size="small" />
-                    </IconButton>
-                  </ListSubheader>
-                  {Offer ? (
-                    <>
-                      <Box
+                    <Typography variant="subtitle1" component="h4">
+                      Estado del Envio:
+                    </Typography>
+                    <Typography variant="body2" component="p">
+                      Esperando a que el transportista reciba los productos
+                    </Typography>
+                  </Box>
+                  <Box
+                    component={Paper}
+                    variant="outlined"
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      margin: "1rem 0",
+                      padding: "2rem",
+                      gap: "1rem",
+                    }}
+                  >
+                    <Typography variant="subtitle1" component="h4">
+                      ¿Confirma que ha entregado los Productos?
+                    </Typography>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        gap: "1rem",
+                      }}
+                    >
+                      <Button variant="outlined"> No </Button>
+                      <Button variant="outlined"> Si </Button>
+                    </Box>
+                  </Box>
+                </>
+              ) : (
+                <>
+                  <Box
+                    sx={{
+                      padding: "1rem",
+                    }}
+                  >
+                    <List
+                      sx={{
+                        width: "100%",
+                        bgcolor: "background.paper",
+                      }}
+                      component={Paper}
+                      variant="outlined"
+                    >
+                      <ListSubheader
                         sx={{
-                          margin: "2rem 3rem",
+                          display: "flex",
+                          alignContent: "center",
+                          justifyContent: "center",
+                          alignItems: "center",
                         }}
                       >
-                        <ListSubheader>
-                          Ofrecer Servicios de Transporte
-                        </ListSubheader>
-                        <FormControl fullWidth>
-                          <InputLabel id="offer">Sus Servicios</InputLabel>
-                          <Select
-                            labelId="offer"
-                            id="offer"
-                            variant="filled"
-                            value={SelectedOffer}
-                            label="Sus Servicios"
-                            onChange={handleSelectedOffer}
-                          >
-                            <MenuItem value={1}>Carro</MenuItem>
-                            <MenuItem value={2}>Moto</MenuItem>
-                            <MenuItem value={3}>Camion</MenuItem>
-                          </Select>
-                        </FormControl>
-                        <Box
-                          sx={{
-                            margin: ".5rem 0",
-                            display: "flex",
-                            alignContent: "center",
-                            justifyContent: "space-around",
-                            alignItems: "center",
-                          }}
-                        >
-                          <Button
-                            onClick={handleCloseOffer}
-                            variant="outlined"
+                        Servicios de Transporte Ofertados{" "}
+                        <IconButton onClick={handleOffer}>
+                          <BookmarkAddTwoToneIcon
                             color="secondary"
+                            size="small"
+                          />
+                        </IconButton>
+                      </ListSubheader>
+                      {Offer ? (
+                        <>
+                          <Box
+                            sx={{
+                              margin: "2rem 3rem",
+                            }}
                           >
-                            Cancelar
-                          </Button>
-                          <Button variant="outlined" color="secondary">
-                            Ofrecer Servicio
-                          </Button>
-                        </Box>
-                      </Box>
-                    </>
-                  ) : null}
-                  <Divider />
-                  <ListItem
-                    divider
-                    secondaryAction={
-                      <IconButton edge="end" aria-label="Eliminar Ofrecer">
-                        <DeleteTwoToneIcon color="error" size="small" />
-                      </IconButton>
-                    }
-                    disablePadding
-                  >
-                    <ListItemButton role={undefined}>
-                      <ListItemIcon>
-                        <BookmarkAddedTwoToneIcon color="secondary" />
-                      </ListItemIcon>
-                      <ListItemText primary="Prueba 1" secondary="prueba 2" />
-                    </ListItemButton>
-                  </ListItem>
-                </List>
-              </Box>
+                            <ListSubheader>
+                              Ofrecer Servicios de Transporte
+                            </ListSubheader>
+                            <FormControl fullWidth>
+                              <InputLabel id="offer">Sus Servicios</InputLabel>
+                              <Select
+                                labelId="offer"
+                                id="offer"
+                                variant="filled"
+                                value={SelectedOffer}
+                                label="Sus Servicios"
+                                onChange={handleSelectedOffer}
+                              >
+                                <MenuItem value={1}>Carro</MenuItem>
+                                <MenuItem value={2}>Moto</MenuItem>
+                                <MenuItem value={3}>Camion</MenuItem>
+                              </Select>
+                            </FormControl>
+                            <Box
+                              sx={{
+                                margin: ".5rem 0",
+                                display: "flex",
+                                alignContent: "center",
+                                justifyContent: "space-around",
+                                alignItems: "center",
+                              }}
+                            >
+                              <Button
+                                onClick={handleCloseOffer}
+                                variant="outlined"
+                                color="secondary"
+                              >
+                                Cancelar
+                              </Button>
+                              <Button variant="outlined" color="secondary">
+                                Ofrecer Servicio
+                              </Button>
+                            </Box>
+                          </Box>
+                        </>
+                      ) : null}
+                      <Divider />
+                      <ListItem
+                        divider
+                        secondaryAction={
+                          <IconButton edge="end" aria-label="Eliminar Ofrecer">
+                            <DeleteTwoToneIcon color="error" size="small" />
+                          </IconButton>
+                        }
+                        disablePadding
+                      >
+                        <ListItemButton role={undefined}>
+                          <ListItemIcon>
+                            <BookmarkAddedTwoToneIcon color="secondary" />
+                          </ListItemIcon>
+                          <ListItemText
+                            primary="Prueba 1"
+                            secondary="prueba 2"
+                          />
+                        </ListItemButton>
+                      </ListItem>
+                    </List>
+                  </Box>
+                </>
+              )}
             </Box>
           </Paper>
         </Box>
@@ -251,7 +326,7 @@ function EnviosDetails() {
             <TableContainer
               component={Paper}
               variant="outlined"
-              sx={{ margin: "2rem 0" }}
+              sx={{ margin: "1rem 0" }}
             >
               <Table>
                 <TableBody>
@@ -302,6 +377,110 @@ function EnviosDetails() {
                 </TableBody>
               </Table>
             </TableContainer>
+            {TransportistaAccepted ? (
+              <>
+                <Box
+                  sx={{
+                    padding: "1rem",
+                  }}
+                  variant="outlined"
+                >
+                  <Typography align="center" variant="h4" component="h2">
+                    Detalles del Servicio de Transporte
+                  </Typography>
+                  <TableContainer
+                    component={Paper}
+                    variant="outlined"
+                    sx={{ margin: "1rem 0" }}
+                  >
+                    <Table>
+                      <TableBody>
+                        <TableRow>
+                          <TableCell component="th">
+                            Medio de Transporte
+                          </TableCell>
+                          <TableCell align="center">Carro</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell component="th">
+                            Nombre del Vehiculo
+                          </TableCell>
+                          <TableCell align="center">Weishler Joice</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell component="th">Horario</TableCell>
+                          <TableCell align="center">De 7am a 8pm</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell component="th">Precio</TableCell>
+                          <TableCell align="center">50$</TableCell>
+                        </TableRow>
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                </Box>
+                <Box
+                  sx={{
+                    padding: "1rem",
+                  }}
+                  variant="outlined"
+                >
+                  <Typography align="center" variant="h4" component="h2">
+                    Detalles del Transportista
+                  </Typography>
+                  <TableContainer
+                    component={Paper}
+                    variant="outlined"
+                    sx={{ margin: "1rem 0" }}
+                  >
+                    <Table>
+                      <TableBody>
+                        <TableRow>
+                          <TableCell component="th">Nombres</TableCell>
+                          <TableCell align="center">Weishler Joice</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell component="th">Apellidos</TableCell>
+                          <TableCell align="center">Berman Torres</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell component="th">Telefonos</TableCell>
+                          <TableCell align="center">
+                            <IconButton
+                              size="small"
+                              onClick={() => setOpen(!open)}
+                            >
+                              {open ? (
+                                <KeyboardArrowUpIcon />
+                              ) : (
+                                <KeyboardArrowDownIcon />
+                              )}
+                            </IconButton>
+                            <Collapse in={open} timeout="auto" unmountOnExit>
+                              <Box sx={{ margin: 1 }}>
+                                <Table size="small" aria-label="purchases">
+                                  <TableBody>
+                                    <TableRow>
+                                      <TableCell>04242029818</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                      <TableCell>04242596061</TableCell>
+                                    </TableRow>
+                                  </TableBody>
+                                </Table>
+                              </Box>
+                            </Collapse>
+                          </TableCell>
+                        </TableRow>
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                  <Box sx={{ display: "flex", justifyContent: "center" }}>
+                    <Button variant="outlined">Cancelar Servicio</Button>
+                  </Box>
+                </Box>
+              </>
+            ) : null}
           </Paper>
         </Box>
       </Box>
